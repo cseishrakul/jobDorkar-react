f683d33e1a04afe8616f4218bab427b224a4b4cc
cseishrakul <ishrakul236@gmail.com>
2025-05-04 17:10:20 +0600

    Updated

--- src/components/dashboard/profile/Profile.jsx
+++ src/components/dashboard/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import User from './User'
 import Shortcuts from './Shortcuts'
 import DonutChart from './DonutChart'
